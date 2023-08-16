55c6225747f6bc535055954485eed09f9f1eb30a
Melquisedeque (Megas) do Nascimento <71480330+Megas-MDN@users.noreply.github.com>
2023-08-15 22:40:22 -0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className='content'>
       <img src={srcImage} alt="My-foto" className='my-image'/>
         <p>Hi, I'm Megas, nice to meet you</p>
-        <h1>Frontend Developer</h1>
+        <h1>Full Stack Developer</h1>
       <div>
         <Link to='/projects' className='btn'>Projects</Link>
         <Link to='contact' className='btn btn-light'>Contact</Link>
